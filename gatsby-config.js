c1107cc04a7134bd014d3488b764ff2b10c21c9e
Jeff Rafter <jeffrafter@gmail.com>
2019-02-22 19:52:35 -0800

    Add in remark collection

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-remark-collection`
   ]
 };
